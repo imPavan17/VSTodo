e5231be51741ea6cef72cfc98a9c484d42c38469
Pavan <pavans@geekyants.com>
2021-09-07 10:51:15 +0530

    sidebar setup

--- src/extension.ts
+++ src/extension.ts
@@ -2,12 +2,18 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import { HelloWorldPanel } from "./HelloWorldPanel";
+import { SidebarProvider } from "./SidebarProvider";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
   // Shows in a debug console
   console.log('Congratulations, your extension "vstodo" is now active!');
+
+  const sidebarProvider = new SidebarProvider(context.extensionUri);
+  context.subscriptions.push(
+    vscode.window.registerWebviewViewProvider("vstodo-sidebar", sidebarProvider)
+  );
 
   context.subscriptions.push(
     vscode.commands.registerCommand("vstodo.helloWorld", function () {
---
+++ webviews/components/Sidebar.svelte
@@ -0,0 +1,7 @@
+<style>
+    div {
+        color: pink;
+    }
+</style>
+
+<div>Hello from sidebar 1</div>
